822e2fe4b30d40f8827238e60cbb47573e479d58
MbuviDev <mbuvi.meshack99@gmail.com>
2024-06-18 10:06:29 +0300

    First commit

---
+++ dog.js
@@ -0,0 +1,15 @@
+let myAge = 24
+
+let earlyYears = 2
+earlyYears = earlyYears * 10.5
+
+let laterYears = myAge - 2
+laterYears = laterYears * 4
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let MyAgeInDogYears = earlyYears + laterYears
+
+let myName = "Mbuvi".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
